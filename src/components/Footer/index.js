50a6a8c0fa03e172f8abcaf9824f2edeb3ed2c76
Nguyenleminhbao-tt5 <bao.nguyenminhbaott5@hcmut.edu.vn>
2022-10-20 22:26:10 +0700

    add footer

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -1,12 +1,79 @@
 import classNames from "classnames/bind";
 import styles from './Footer.module.scss';
-
+import '../base.module.scss';
 const cx=classNames.bind(styles);
 
 function Footer()
 {
     return(
-        <div>Footer</div>
+        <footer className={cx('wrapper')}>
+            <div className={cx('grid')}>
+                <div className={cx('grid__row')}>
+                    <div className={cx('grid__column-2-4')}>
+                        <h3 className={cx('footer__heading')}>Về Web Shoes</h3>
+                        <ul className={cx('footer__list')}>
+                            <li className={cx('footer__list-item')}>
+                                <a href="" className={cx('footer__list-item_link')} >Giới thiệu chung</a>
+                            </li>
+                            <li className={cx('footer__list-item')}>
+                                <a href="" className={cx('footer__list-item_link')} >Tuyển dụng</a>
+                            </li>
+                        </ul>
+                    </div>
+                    <div className={cx('grid__column-2-4')}>
+                    <h3 className={cx('footer__heading')}>Liên Hệ</h3>
+                        <ul className={cx('footer__list')}>
+                            <li className={cx('footer__list-item')}>
+                                <i className={cx('footer__list-icon','fa-solid fa-phone')}></i>
+                                <a href="" className={cx('footer__list-item_link')} >0398841276</a>
+                            </li>
+                            <li className={cx('footer__list-item')}>
+                                <i className={cx('footer__list-icon','fa-regular fa-envelope')}> </i>
+                                <a href="" className={cx('footer__list-item_link')} >bao.nguyenminhbaott5@hcmut.edu.vn</a>
+                            </li>
+                            <li className={cx('footer__list-item')}>
+                                <i className={cx('footer__list-icon','fa-solid fa-location-dot')}></i>
+                                <a href="" className={cx('footer__list-item_link')} >268 Lý Thường Kiệt, Phường 14, Quận 10, Tp Hồ Chí Minh</a>
+                            </li>
+                        </ul>
+                    </div>
+                    <div className={cx('grid__column-2-4')}>
+                        <h3 className={cx('footer__heading')}>Hỗ Trợ Khách Hàng</h3>
+                        <ul className={cx('footer__list')}>
+                            <li className={cx('footer__list-item')}>
+                                <a href="" className={cx('footer__list-item_link')} >Mua hàng từ xa</a>
+                            </li>
+                            <li className={cx('footer__list-item')}>
+                                <a href="" className={cx('footer__list-item_link')} >Quy định đổi trả</a>
+                            </li>
+                        </ul>
+                        <div className={cx('footer__wrap')}>
+                            <div className={cx('transportation')}>
+
+                            </div>
+                            <div className={cx('payment')}>
+                                
+                            </div>
+                        </div>
+                    </div>
+                    <div className={cx('grid__column-2-4')}>
+                        <h3 className={cx('footer__heading')}>Dịch Vụ Cung Cấp</h3>
+                        <ul className={cx('footer__list')}>
+                            <li className={cx('footer__list-item')}>
+                                <a href="" className={cx('footer__list-item_link')} >Dịch vụ sửa chữa</a>
+                            </li>
+                            <li className={cx('footer__list-item')}>
+                                <a href="" className={cx('footer__list-item_link')} >Nâng cấp phần cứng</a>
+                            </li>
+                            <li className={cx('footer__list-item')}>
+                                <a href="" className={cx('footer__list-item_link')} >Bảo hành sản phẩm</a>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+            <div className={cx('footer__copy')}>Copyright 2022 </div>
+        </footer>
     );
 }
 
--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,7 +1,6 @@
 import classNames from "classnames/bind";
 import styles from "./Header.module.scss";
-import '../base.module.scss'
-
+import '../base.module.scss';
 const cx=classNames.bind(styles);
 
 function Header()
@@ -18,35 +17,40 @@ function Header()
                     </div>
                    
                 </div>
-                <div className={cx('navbar1__cart')}>
-                    <i className={cx('navbar1__cart-icon','fa-solid fa-cart-shopping')}></i>
-                    <a href="" className={cx('navbar1__cart-link')}>Giỏ hàng</a>
-                </div>
-                <a href="" className={cx('navbar1__login')}>Đăng nhập</a>
-                <div className={cx('navbar1__social')}>
-                    <a href="" className={cx('navbar1__social-fb')}>
-                        <i className={cx('navbar1__social-icon_fb fa-brands fa-facebook')}></i>
-                    </a>
-                    <a href="" className={cx('navbar1__social-insta')}>
-                        <i className={cx('navbar1__social-icon_insta fa-brands fa-instagram')}></i>
-                    </a>
-                </div>
+                <div className={cx('navbar1__wrap')}>
+                    <div className={cx('navbar1__cart')}>
+                        <i className={cx('navbar1__cart-icon','fa-solid fa-cart-shopping')}></i>
+                        <a href="" className={cx('navbar1__cart-link')}>Giỏ hàng</a>
+                    </div>
+                    <a href="" className={cx('navbar1__login')}>Đăng nhập</a>
+                    <div className={cx('navbar1__social')}>
+                        <a href="" className={cx('navbar1__social-item')}>
+                            <i className={cx('navbar1__social-icon_fb','fa-brands fa-facebook')}></i>
+                        </a>
+                        <a href="" className={cx('navbar1__social-item')}>
+                            <i className={cx('navbar1__social-icon_insta','fa-brands fa-instagram')}></i>
+                        </a>
+                    </div>
+                </div>             
             </div>
             <div className={cx('navbar2')}>
                 <div className={cx('navbar2__category')}>
-                    <i className={cx('navbar2__category-icon_first fa-solid fa-bars')}></i>
+                    <i className={cx('navbar2__category-icon_first','fa-solid fa-bars')}></i>
                     <div className={cx('navbar2__category-text')}>DANH MỤC SẢN PHẨM</div>
-                    <i className={cx('navbar2__category-icon_second fa-solid fa-angle-down')}></i>
+                    <i className={cx('navbar2__category-icon_second','fa-solid fa-angle-down')}></i>
                 </div>
-                <div className={cx('navbar2__searchProduct')}>
-                    <i className={cx('navbar2__searchProduct-icon fa-solid fa-truck-fast')}></i>
-                    <a href="" className={cx('navbar2__searchProduct-text')}>Tra cứu đơn hàng</a>
+                <div className={cx('navbar2__wrap')}>
+                    <div className={cx('navbar2__searchProduct')}>
+                        <i className={cx('navbar2__searchProduct-icon','fa-solid fa-truck-fast')}></i>
+                        <a href="" className={cx('navbar2__searchProduct-text')}>Tra cứu đơn hàng</a>
+                    </div>
+                    <a href="" className={cx('navbar2__intro')}>Giới thiệu</a>
+                    <div className={cx('navbar2__contact')}>
+                        <i className={cx('navbar2__contact-icon','fa-solid fa-phone')}></i>
+                        <span className={cx('navbar2__contact-text')}>Liên hệ</span>
+                    </div>
                 </div>
-                <a href="" className={cx('navbar2__intro')}>Giới thiệu</a>
-                <div className={cx('navbar2__contact')}>
-                    <i className={cx('navbar2__contact-icon fa-solid fa-phone')}></i>
-                    <span className={cx('navbar2__contact-text')}>Liên hệ</span>
-                </div>
+                
             </div>
         </header>
     );
